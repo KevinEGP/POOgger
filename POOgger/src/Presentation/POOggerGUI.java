901bb7f932ca86d35c1cbc4e542f2675443e8560
alism <alism@LAPTOP-9N3HRKJS>
2020-11-12 19:03:34 -0500

    Se añadió soporte inicial para hilos.

--- POOgger/src/Presentation/POOggerGUI.java
+++ POOgger/src/Presentation/POOggerGUI.java
@@ -3,15 +3,19 @@ package Presentation;
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Dimension;
-import javax.swing.JFrame;
 
-public class POOggerGUI extends Canvas {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class POOggerGUI extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 	private static final int WIDTH = 745, HEIGHT = 745;
-	private static final String WINDOWNAME = "POOGER";
+	private static final String WINDOWNAME = "POOGGER";
 	private static JFrame windowGame;
-	
+
+	private static Thread thread;
+
 	private POOggerGUI() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		windowGame = new JFrame(WINDOWNAME);
@@ -23,8 +27,23 @@ public class POOggerGUI extends Canvas {
 		windowGame.setLocationRelativeTo(null);
 		windowGame.setVisible(true);
 	}
-	
+
+	private void begin() {
+		thread = new Thread(this, "GUI Execution");
+		thread.start();
+	}
+
+	private void stop() {
+
+	}
+
+	@Override
+	public void run() {
+		JOptionPane.showMessageDialog(this, "Thread is Active");
+	}
+
 	public static void main(String[] args) {
 		POOggerGUI pooggerGUI = new POOggerGUI();
+		pooggerGUI.begin();
 	}
 }
